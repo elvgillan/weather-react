9bf8131cbc7e1bd9f11c30943f1306f0e3254aef
elvgillan <98256076+elvgillan@users.noreply.github.com>
2023-04-30 15:49:22 +1000

    Updated Footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,15 +5,27 @@ export default function Footer() {
   return (
     <div className="Footer">
       This project was coded by{" "}
-      <a href="https://www.linkedin.com/in/eringillan/" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/eringillan/"
+        target="_blank"
+        rel="noreferrer"
+      >
         Erin Gillan
       </a>{" "}
       and is{" "}
-      <a href="https://github.com/elvgillan/my-weather-app" target="_blank">
+      <a
+        href="https://github.com/elvgillan/my-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
         open-sourced on GitHub
       </a>{" "}
       and{" "}
-      <a href="https://dreamy-visvesvaraya-8d7417.netlify.app/" target="_blank">
+      <a
+        href="https://dreamy-visvesvaraya-8d7417.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
         hosted on Netlify
       </a>
     </div>
